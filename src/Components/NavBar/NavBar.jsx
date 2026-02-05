2852b7de063e65e80dc0f4a315bad289187d535c
Facundo Lentini <lentinifacundo@gmail.com4>
2026-02-05 02:00:52 -0300

    fixin menu [preview]

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
             <div className='border-2 border-white w-[90%]'></div>
             <div className='border-2 border-white w-[80%]'></div>
         </div>
-        <ul className={`flex flex-col gap-5 text-md ${isOpen ? 'hidden' : ''}
+        <ul className={`flex flex-col gap-5 text-md ${isOpen ? '' : 'hidden'}
             md:flex-row md:flex md:text-sm`}>
             <li><a className='text- black font-jakarta transition-colors duration-300 ease-in-out font-semibold hover:text-primary
                 md: text-xs' onClick={toggleMenu} href="#home">Inicio</a></li>
